86863fb2cd252369607a8905077d849258689ead
unknown <slisette127@gmail.com>
2021-11-18 19:10:40 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LogIn from './Components/LogIn';
 import NavBar from './Components/NavBar';
+import NewUser from './Components/NewUser'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <NewUser />
      <div><LogIn/></div>
     </div>
   );
